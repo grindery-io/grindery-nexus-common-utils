21711381f10d978911da52fe61a109b2ddc1a432
SAPikachu <sapikachu@gmail.com>
2022-08-27 16:50:52 +0800

    Fix JsonRpcWebSocket

--- src/ws.ts
+++ src/ws.ts
@@ -17,12 +17,36 @@ export class JsonRpcWebSocket {
   private serverAndClient: JSONRPCServerAndClient;
   private ws: WebSocket;
 
-  constructor(url: string) {
+  constructor(url: string, private requestTimeout = 60000) {
     this.ws = new WebSocket(url);
     this.serverAndClient = new JSONRPCServerAndClient(
       new JSONRPCServer(),
       new JSONRPCClient(async (request) => {
-        this.ws.send(JSON.stringify(request));
+        if (this.ws.readyState === WebSocket.OPEN) {
+          this.ws.send(JSON.stringify(request));
+          return;
+        }
+        if (this.ws.readyState !== WebSocket.CONNECTING) {
+          throw new Error("WebSocket is not open");
+        }
+        await new Promise((resolve, reject) => {
+          const onError = function (e) {
+            reject(e || new Error("Failed to send request to WebSocket"));
+          };
+          this.ws.on("error", onError);
+          this.ws.on("close", onError);
+          this.ws.once("open", () => {
+            this.ws.off("error", onError);
+            this.ws.off("close", onError);
+            try {
+              this.ws.send(JSON.stringify(request));
+            } catch (error) {
+              reject(error);
+              return;
+            }
+            resolve(undefined);
+          });
+        });
       })
     );
     this.ws.onmessage = (event) => {
@@ -44,7 +68,27 @@ export class JsonRpcWebSocket {
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   request<T extends JSONRPCParams, U = unknown>(method: string, params?: T, clientParams?: any): PromiseLike<U> {
-    return this.serverAndClient.timeout(10000).request(method, params, clientParams);
+    let running = true;
+    const keepAlive = () => {
+      if (!running) {
+        return;
+      }
+      setTimeout(() => {
+        if (!running) {
+          return;
+        }
+        this.serverAndClient
+          .timeout(5000)
+          .request("ping")
+          .then(keepAlive, () => console.error(`WebSocket connection is closed while calling ${method}`));
+      }, 5000);
+    };
+    keepAlive();
+    try {
+      return this.serverAndClient.timeout(this.requestTimeout).request(method, params, clientParams);
+    } finally {
+      running = false;
+    }
   }
   close() {
     this.ws.close();
